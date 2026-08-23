595b5dd86cb7a3445c8569492369ce2df1b4e22e
mentako-ya <mentakoya@gmail.com>
2026-08-23 13:43:04 +0900

    fix(display): use hid mouse report for trackball status widget

--- boards/shields/dongle_display/widgets/trackball_status.c
+++ boards/shields/dongle_display/widgets/trackball_status.c
@@ -9,7 +9,7 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 #include <zmk/display.h>
 #include <zmk/event_manager.h>
-#include <zmk/pointing/input_split.h>
+#include <zmk/hid.h>
 
 #include "trackball_status.h"
 #include "/Users/tools/git/zmk-config-mtk64/config/boards/shields/mtk64/paw32xx_cpi_changed.h"
@@ -18,20 +18,20 @@ static sys_slist_t widgets = SYS_SLIST_STATIC_INIT(&widgets);
 
 struct trackball_state {
     uint16_t cpi;
-    int16_t dx;
-    int16_t dy;
 };
 
 static uint16_t g_cpi = 1600;
-static int16_t g_dx = 0;
-static int16_t g_dy = 0;
 
 static void set_trackball_labels(struct zmk_widget_trackball_status *widget, struct trackball_state state) {
     if (!widget || !widget->cpi_label || !widget->xy_label) {
         return;
     }
     lv_label_set_text_fmt(widget->cpi_label, "CPI:%u", state.cpi);
-    lv_label_set_text_fmt(widget->xy_label, "X:%+3d Y:%+3d", state.dx, state.dy);
+    // Dynamic XY display from current HID mouse state
+    struct zmk_hid_mouse_report *report = zmk_hid_get_mouse_report();
+    if (report) {
+        lv_label_set_text_fmt(widget->xy_label, "X:%+3d Y:%+3d", report->x, report->y);
+    }
 }
 
 static void trackball_status_update_cb(struct trackball_state state) {
@@ -47,20 +47,8 @@ static struct trackball_state trackball_status_get_state(const zmk_event_t *eh) 
         g_cpi = cpi_ev->cpi;
     }
 
-    const struct zmk_input_split_report_peripheral_event *input_ev =
-        as_zmk_input_split_report_peripheral_event(eh);
-    if (input_ev) {
-        if (input_ev->code == 0) { // REL_X
-            g_dx = (int16_t)input_ev->value;
-        } else if (input_ev->code == 1) { // REL_Y
-            g_dy = (int16_t)input_ev->value;
-        }
-    }
-
     return (struct trackball_state){
         .cpi = g_cpi,
-        .dx = g_dx,
-        .dy = g_dy,
     };
 }
 
@@ -68,7 +56,12 @@ ZMK_DISPLAY_WIDGET_LISTENER(widget_trackball_status, struct trackball_state,
                             trackball_status_update_cb, trackball_status_get_state)
 
 ZMK_SUBSCRIPTION(widget_trackball_status, zmk_paw32xx_cpi_changed);
-ZMK_SUBSCRIPTION(widget_trackball_status, zmk_input_split_report_peripheral_event);
+
+// Periodic UI refresh timer for smooth XY delta updates
+static void trackball_ui_timer_cb(lv_timer_t *timer) {
+    struct trackball_state state = {.cpi = g_cpi};
+    trackball_status_update_cb(state);
+}
 
 int zmk_widget_trackball_status_init(struct zmk_widget_trackball_status *widget, lv_obj_t *parent) {
     widget->obj = lv_obj_create(parent);
@@ -85,6 +78,8 @@ int zmk_widget_trackball_status_init(struct zmk_widget_trackball_status *widget,
     sys_slist_append(&widgets, &widget->node);
 
     widget_trackball_status_init();
+
+    lv_timer_create(trackball_ui_timer_cb, 100, NULL);
 
     return 0;
 }
