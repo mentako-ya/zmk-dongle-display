878d55895092cae420d1c0603bdb83bdee828b98
mentako-ya <mentakoya@gmail.com>
2026-09-12 22:33:24 +0900

    fix(display): レイヤー表示の垂直位置(Yオフセット)を他アイコンのベースライン・中心に統一
    - layer_status_widget のYオフセットを -5px に調整
    - モディファイアおよびAuto/Scrollアイコンの描画下端(ベースライン: y=59)および垂直中心と完全に整合

--- boards/shields/dongle_display/custom_status_screen.c
+++ boards/shields/dongle_display/custom_status_screen.c
@@ -96,7 +96,7 @@ lv_obj_t *zmk_display_status_screen() {
     zmk_widget_layer_status_init(&layer_status_widget, screen);
     lv_obj_set_width(zmk_widget_layer_status_obj(&layer_status_widget), 38);
     lv_obj_set_style_text_align(zmk_widget_layer_status_obj(&layer_status_widget), LV_TEXT_ALIGN_RIGHT, 0);
-    lv_obj_align(zmk_widget_layer_status_obj(&layer_status_widget), LV_ALIGN_BOTTOM_RIGHT, 0, 0);
+    lv_obj_align(zmk_widget_layer_status_obj(&layer_status_widget), LV_ALIGN_BOTTOM_RIGHT, 0, -5);
 #endif
 
 #if IS_ENABLED(CONFIG_ZMK_BATTERY) || IS_ENABLED(CONFIG_ZMK_BATTERY_REPORTING)
