f8d5ee4576d72c982ac6a906d5afd54cc4f890d3
mentako-ya <mentakoya@gmail.com>
2026-09-12 22:31:47 +0900

    fix(display): モディファイアとAuto/Scrollアイコンの間隔均等化およびレイヤー表示の右端配置
    - ShiftとAutoの間隔をモディファイア間(1px)と同一になるようオフセット調整(-1px)
    - Ctrl, Opt, Cmd, Shift, Auto, Scroll の全6アイコンを完全等間隔配置に統一
    - レイヤー名表示の幅を38px・右揃え(LV_TEXT_ALIGN_RIGHT)にし、画面最右端(LV_ALIGN_BOTTOM_RIGHT, 0, 0)に寄せて配置

--- boards/shields/dongle_display/custom_status_screen.c
+++ boards/shields/dongle_display/custom_status_screen.c
@@ -86,7 +86,7 @@ lv_obj_t *zmk_display_status_screen() {
     lv_obj_align_to(zmk_widget_hid_indicators_obj(&hid_indicators_widget), zmk_widget_modifiers_obj(&modifiers_widget), LV_ALIGN_OUT_TOP_LEFT, 0, -2);
 #endif
     zmk_widget_aml_scr_status_init(&aml_scr_status_widget, screen);
-    lv_obj_align_to(zmk_widget_aml_scr_status_obj(&aml_scr_status_widget), zmk_widget_modifiers_obj(&modifiers_widget), LV_ALIGN_OUT_RIGHT_BOTTOM, 2, 0);
+    lv_obj_align_to(zmk_widget_aml_scr_status_obj(&aml_scr_status_widget), zmk_widget_modifiers_obj(&modifiers_widget), LV_ALIGN_OUT_RIGHT_BOTTOM, -1, 0);
 #else
     zmk_widget_aml_scr_status_init(&aml_scr_status_widget, screen);
     lv_obj_align(zmk_widget_aml_scr_status_obj(&aml_scr_status_widget), LV_ALIGN_BOTTOM_LEFT, 0, 0);
@@ -94,6 +94,8 @@ lv_obj_t *zmk_display_status_screen() {
 
 #if IS_ENABLED(CONFIG_ZMK_DONGLE_DISPLAY_LAYER)
     zmk_widget_layer_status_init(&layer_status_widget, screen);
+    lv_obj_set_width(zmk_widget_layer_status_obj(&layer_status_widget), 38);
+    lv_obj_set_style_text_align(zmk_widget_layer_status_obj(&layer_status_widget), LV_TEXT_ALIGN_RIGHT, 0);
     lv_obj_align(zmk_widget_layer_status_obj(&layer_status_widget), LV_ALIGN_BOTTOM_RIGHT, 0, 0);
 #endif
 
